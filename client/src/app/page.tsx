3bc3cc29e9e3ec01cbf332ed54522789ea7977ce
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2024-09-22 21:43:49 +0700

    feat(client): test chart and dummy data

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,95 +1,44 @@
-import Image from "next/image";
+"use client";
+import "chart.js/auto";
+import { Line } from "react-chartjs-2";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+// TODO: mocks
+const dummyData = {
+  labels: [
+    "1 week ago", 
+    "now",
+  ],
+  datasets: [
+    {
+      label: "previous price",
+      data: [9.50, 95.00], 
+      fill: false,
+      borderColor: "rgba(255, 99, 132, 1)", 
+      tension: 0.1,
+    },
+    {
+      label: "updated price",
+      data: [10.20, 102.10], 
+      fill: false,
+      borderColor: "rgba(75, 192, 192, 1)",
+      tension: 0.1,
+    },
+  ],
+};
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+// TODO: get data from repo and map to chart
+// add some kind of form also to put names and fetch 
+const Home: React.FC = () => {
+  return (
+    <div>
+      <div className={styles.chart}>
+        <Line data={dummyData} />
+      </div>
+      <p className={styles.footer}>padulkemid farmio saas-test</p>
     </div>
   );
-}
+};
+
+export default Home;
+
